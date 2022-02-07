876036c4f82d437546dbd124baf3826fd73d6b34
Matthias Kestenholz <mk@feinheit.ch>
2022-02-07 13:56:47 +0100

    Split the testsuite

--- test_speckenv_django_cache_url.py
+++ test_speckenv_django_cache_url.py
@@ -1,68 +1,6 @@
 from unittest import TestCase
 
-from speckenv_django import django_cache_url, django_database_url
-
-
-# EMAIL_URL=submission://no-reply@example_com:8p7f%21Y%40do6@smtp.mailgun.com:587/
-
-
-class DjangoDatabaseURLTest(TestCase):
-    def test_parse_database_url(self):
-        url = "postgres://example_com:feqcv97siqxwu1@localhost:5432/example_com"
-        self.assertEqual(
-            django_database_url(url),
-            {
-                "ENGINE": "django.db.backends.postgresql",
-                "NAME": "example_com",
-                "USER": "example_com",
-                "PASSWORD": "feqcv97siqxwu1",
-                "HOST": "localhost",
-                "PORT": "5432",
-            },
-        )
-
-    def test_parse_local_database_url(self):
-        url = "postgres://localhost:5432/example_com"
-        self.assertEqual(
-            django_database_url(url),
-            {
-                "ENGINE": "django.db.backends.postgresql",
-                "NAME": "example_com",
-                "USER": "",
-                "PASSWORD": "",
-                "HOST": "localhost",
-                "PORT": "5432",
-            },
-        )
-
-    def test_parse_quoted_database_url(self):
-        url = "mysql://%23user:%23password@ec2.amazonaws.com:5431/%23database"
-        self.assertEqual(
-            django_database_url(url),
-            {
-                "ENGINE": "django.db.backends.mysql",
-                "NAME": "#database",
-                "USER": "#user",
-                "PASSWORD": "#password",
-                "HOST": "ec2.amazonaws.com",
-                "PORT": "5431",
-            },
-        )
-
-    def test_parse_max_age_database_url(self):
-        url = "postgres://localhost:5432/example_com?conn_max_age=10"
-        self.assertEqual(
-            django_database_url(url),
-            {
-                "ENGINE": "django.db.backends.postgresql",
-                "NAME": "example_com",
-                "USER": "",
-                "PASSWORD": "",
-                "HOST": "localhost",
-                "PORT": "5432",
-                "CONN_MAX_AGE": 10,
-            },
-        )
+from speckenv_django import django_cache_url
 
 
 class DjangoCacheURLTest(TestCase):
