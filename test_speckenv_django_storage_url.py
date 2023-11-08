cce57b8929443e034bd495e7f52f836ea1bb1fb9
Matthias Kestenholz <mk@feinheit.ch>
2023-11-08 15:33:02 +0100

    Experiment with parsing S3 URLs for a different S3 service

--- test_speckenv_django_storage_url.py
+++ test_speckenv_django_storage_url.py
@@ -4,6 +4,8 @@ from speckenv_django import InvalidURLError, django_storage_url
 
 
 class DjangoStorageURLTest(TestCase):
+    maxDiff = None
+
     def test_parse_storage_url(self):
         url = "file:///test/media/"
         self.assertEqual(
@@ -49,3 +51,21 @@ class DjangoStorageURLTest(TestCase):
     def test_invalid_s3(self):
         with self.assertRaises(InvalidURLError):
             django_storage_url("s3://s3.eu-central-1.amazonaws.com/bucket")
+
+    def test_other_s3(self):
+        # https://docs.nine.ch/docs/object-storage/object-storage-client-tools
+        url = "s3://6aaf50b17357446bb:fcf1c9c6bc5c43@bucket-etj4mwc.s3.nine-cz42.amazonaws.com?aws_s3_endpoint_url=https://cz42.objectstorage.nineapis.ch"
+        self.assertEqual(
+            django_storage_url(url),
+            {
+                "BACKEND": "django_s3_storage.storage.S3Storage",
+                "OPTIONS": {
+                    "aws_s3_endpoint_url": "https://cz42.objectstorage.nineapis.ch",
+                    "aws_region": "nine-cz42",
+                    "aws_access_key_id": "6aaf50b17357446bb",
+                    "aws_secret_access_key": "fcf1c9c6bc5c43",
+                    "aws_s3_bucket_name": "bucket-etj4mwc",
+                    "aws_s3_key_prefix": "",
+                },
+            },
+        )
